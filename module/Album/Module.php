06b35ed31b654292b4263b73e74be9b3cbca27a8
Jeff Rafter <jeffrafter@gmail.com>
2014-10-15 16:45:24 -0700

    NEVER DO THIS

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -9,6 +9,50 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 
     const lowercased_consts_are_cool = 1;
 
+    public function smell()
+    {
+        @a();
+        @b("1", "2", "3", "4", "5", "6", "7");
+    }
+
+    public function a()
+    {
+        return "Thing";
+    }
+
+    public function b($p1, $p2, $p3, $p4, $p5, $p6, $p7)
+    {
+      return "";
+    }
+
+    public function c()
+    {
+    }
+
+    public function d()
+    {
+    }
+
+    public function e()
+    {
+    }
+
+    public function f()
+    {
+    }
+
+    public function g()
+    {
+    }
+
+    public function h()
+    {
+    }
+
+    public function aVeryLongMethodNameThatDrivesPeopleCrazyIfTheyDontAutocompleteWhenTyping() {
+      return eval("echo 42");
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
